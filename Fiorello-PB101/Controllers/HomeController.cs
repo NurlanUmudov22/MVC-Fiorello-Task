33bb7008c342de821c5db55abc05e7e74933f9d4
unknown <nurlanmu@code.edu.az>
2024-05-15 01:19:47 +0400

    add hometask mvc

---
+++ Fiorello-PB101/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Fiorello_PB101.Services;
+using Fiorello_PB101.Services.Interfaces;
+using Fiorello_PB101.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fiorello_PB101.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ISliderService _sliderService;
+        private readonly IBlogService _blogService;
+        private readonly IExpertsService _expertsService;
+        public HomeController(ISliderService sliderService,
+                               IBlogService blogService,
+                               IExpertsService expertsService)
+        {
+            _sliderService = sliderService;
+            _blogService = blogService;
+            _expertsService = expertsService;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HomeVM model = new()
+            {
+                Sliders = await _sliderService.GetAllAsync(),
+                SliderInfo = await _sliderService.GetSliderInfoAsync(),
+                Blogs = await _blogService.GetAllAsync(),
+                Experts = await _expertsService.GetAllAsync(),
+            };
+            return View(model);  
+        }
+    }
+}
---
+++ Fiorello-PB101/Data/AppDbContext.cs
@@ -0,0 +1,98 @@
+﻿
+using Fiorello_PB101.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello_PB101.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Slider> Sliders { get; set; }   
+        
+        public DbSet<SliderInfo> SliderInfos { get; set; }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Expert> Experts { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Slider>().HasQueryFilter(m => !m.SoftDeleted);
+ 
+
+
+            modelBuilder.Entity<Blog>().HasData(
+            new Blog
+            {
+                Id = 1,
+                Title = "Title 1",
+                Description = "Reshadin blogu",
+                Image= "blog-feature-img-1.jpg",
+                CreatedDate = DateTime.Now
+            },
+             new Blog
+             {
+                 Id = 2,
+                 Title = "Title 2",
+                 Description = "Kamranin blogu",
+                 Image = "blog-feature-img-3.jpg",
+                 CreatedDate = DateTime.Now
+             },
+              new Blog
+              {
+                  Id = 3,
+                  Title = "Title 3",
+                  Description = "Ilqarin blogu",
+                  Image = "blog-feature-img-4.jpg",
+                  CreatedDate = DateTime.Now
+              }
+                );
+
+
+            modelBuilder.Entity<Expert>().HasData(
+           new Expert
+           {
+               Id = 1,
+               Image = "h3-team-img-1.png ",
+               Name = "CRYSTAL BROOKS",
+               Position = "FLORIST",
+               CreatedDate = DateTime.Now
+           },
+           new Expert
+           {
+               Id = 2,
+               Image = "h3-team-img-2.png ",
+               Name = "SHIRLEY HARRIS",
+               Position = "Manager",
+               CreatedDate = DateTime.Now
+           },
+           new Expert
+           {
+               Id = 3,
+               Image = "h3-team-img-3.png ",
+               Name = "BEVERLY CLARK",
+               Position = "Florist",
+               CreatedDate = DateTime.Now
+           },
+           new Expert
+           {
+               Id = 4,
+               Image = "h3-team-img-4.png ",
+               Name = "AMANDA WATKINS ",
+               Position = "Florist",
+               CreatedDate = DateTime.Now
+           });
+
+        }
+
+
+
+
+
+
+
+    }
+}
